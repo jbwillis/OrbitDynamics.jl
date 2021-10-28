b135f03189c117ddad9acec0611a86b254140841
Jacob Willis <jbwillis272@gmail.com>
2021-10-28 10:57:07 -0400

    tests of M, E, theta

--- test_orbit_representations.jl
+++ test_orbit_representations.jl
@@ -3,7 +3,7 @@ using Test
 include("orbit_dynamics.jl")
 include("orbit_representations.jl")
 
-@testset begin
+@testset "Representation conversions are identity" begin
 	dp = DynamicsParameters()
 	x_cl_1 = [1e4 + dp.R_earth, # a
 			0.5, # e
@@ -19,6 +19,21 @@ include("orbit_representations.jl")
 	@test x_eq_1 ≈ state_vector_to_equinoctial_elements(x_st_1, dp)
 	@test x_st_1 ≈ equinoctial_to_state_vector(x_eq_1, dp)
 
+end
+
+@testset "M, E, theta conversions are identity" begin
+	# M -> E -> theta -> E -> M
+	e = 0.3
+    M0 = pi/3
+    E1 = E_from_M(M0, e)
+    M1 = M_from_E(E1, e)
+    theta = theta_from_E(E1, e)
+    E2 = E_from_theta(theta, e)
+    M2 = M_from_E(E1, e)
+    
+    @test abs(M0 - M1) < 1e-6
+    @test abs(E1 - E2) < 1e-6
+    @test abs(M0 - M2) < 1e-6
 end
 
 
