83b284b591437ad7e09cda279537cae598a39c0d
Jacob Willis <jbwillis272@gmail.com>
2021-11-01 18:51:12 -0400

    Multi-quadrant plot for 3D

--- orbit_plotting.jl
+++ orbit_plotting.jl
@@ -7,18 +7,45 @@ using LaTeXStrings
 function plot_3D_position(r; label=nothing, fig=nothing, ax=nothing)
 	if fig == nothing || ax == nothing
 		fig = plt.figure()
-		ax = Axes3D(fig, auto_add_to_figure=false)
-		ax.scatter3D(0.0, 0.0, 0.0, color="green")
+
+		# 3D in top right
+		ax3D = fig.add_subplot(2, 2, 2, projection="3d")
+		ax3D.scatter3D(0.0, 0.0, 0.0, color="green")
+
+		# 2D in other quadrants
+		ax_ij = fig.add_subplot(2, 2, 1)
+		ax_ij.scatter(0.0, 0.0, color="green")
+		ax_ij.set_xlabel("I")
+		ax_ij.set_ylabel("J")
+
+		ax_ik = fig.add_subplot(2, 2, 3)
+		ax_ik.scatter(0.0, 0.0, color="green")
+		ax_ik.set_xlabel("I")
+		ax_ik.set_ylabel("K")
+
+		ax_jk = fig.add_subplot(2, 2, 4)
+		ax_jk.scatter(0.0, 0.0, color="green")
+		ax_jk.set_xlabel("J")
+		ax_jk.set_ylabel("K")
+
+		ax = [ax_ij, ax3D, ax_ik, ax_jk]
 	end
-    ax.plot3D(r[1,:], r[2,:], r[3,:], label=label)
-    ax.set_title("3D Position")
-    ax.set_xlabel("I")
-    ax.set_ylabel("J")
-    ax.set_zlabel("K")
-    fig.add_axes(ax)
+	ax[2].plot3D(r[1,:], r[2,:], r[3,:], label=label)
+    ax[2].set_title("3D Position")
+    ax[2].set_xlabel("I")
+    ax[2].set_ylabel("J")
+    ax[2].set_zlabel("K")
+
+	ax[1].plot(r[1,:], r[2,:])
+	ax[3].plot(r[1,:], r[3,:])
+	ax[4].plot(r[2,:], r[3,:])
+
+	ax[1].axis("equal")
+	ax[3].axis("equal")
+	ax[4].axis("equal")
 
 	if label != nothing
-		ax.legend()
+		ax[2].legend()
 	end
 
 	return fig, ax
